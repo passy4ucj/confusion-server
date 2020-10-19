1c9475cd544ce1fb56e9d9257f2855bda279159c
passy4ucj <ojinnnakapascal@gmail.com>
2020-10-19 18:23:21 +0100

    Passport

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
+var passport = require('passport');
+var authenticate = require('./authenticate');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -42,6 +45,10 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
@@ -50,11 +57,11 @@ function auth(req, res, next) {
   console.log(req.session);
 
 
-  if (!req.session.user) {
+  if (!req.user) {
     //var authHeader = req.headers.authorization;
 
     var err = new Error('You are not authenticated!');
-    err.status = 401;
+    err.status = 403;
     return next(err);
     
     // var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
@@ -73,14 +80,7 @@ function auth(req, res, next) {
     //   return next(err);
     // }
   } else {
-    if (req.session.user === 'authenticated') {
       next();
-    } else {
-      var err = new Error('You are not authenticated!');
-      
-      err.status = 401;
-      return next(err);
-    }
   }
 
   
